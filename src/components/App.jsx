a9ff913cd73b374195bff564b8af652785ed23f0
Ghazali-Sufi <ghazali4me@gmail.com>
2024-10-01 22:58:17 +0300

    Use arrow function

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,17 +3,6 @@ import { Entry } from "./Entry";
 import { emojipedia } from "../emojipedia";
 import { emojiMeaning } from "../practice";
 
-function createEmoji(emojiTerm) {
-  return (
-    <Entry
-      key={emojiTerm.id}
-      emoji={emojiTerm.emoji}
-      name={emojiTerm.name}
-      meaning={emojiTerm.meaning}
-    />
-  );
-}
-
 export function App() {
   return (
     <div>
@@ -21,7 +10,18 @@ export function App() {
         <span>emojipedia</span>
       </h1>
 
-      <dl className="dictionary">{emojipedia.map(createEmoji)}</dl>
+      <dl className="dictionary">
+        {emojipedia.map((emojiTerm) => {
+          return (
+            <Entry
+              key={emojiTerm.id}
+              emoji={emojiTerm.emoji}
+              name={emojiTerm.name}
+              meaning={emojiTerm.meaning}
+            />
+          );
+        })}
+      </dl>
     </div>
   );
 }
